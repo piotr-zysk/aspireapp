cdc6e1a039c255a8e895e6462af0ff6d890282cd
Piotr Zysk <piotr.zysk@travelcounsellors.com>
2026-03-12 14:37:43 +0100

    direct to swagger dahsboard

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -8,6 +8,8 @@ var appDatabase = sqlServer.AddDatabase("appdb");
 builder.AddProject<Projects.ApiService>("apiservice")
     .WithReference(appDatabase)
     .WaitFor(appDatabase)
+    .WithUrlForEndpoint("http", url => url.Url = "/swagger")
+    .WithUrlForEndpoint("https", url => url.Url = "/swagger")
     .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
